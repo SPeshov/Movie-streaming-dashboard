d7bc430aa93d6ef7e1bd690f0162f69de22f9212
SPeshov <stojan.peshov@gmail.com>
2022-06-14 11:28:30 +0200

    get all movies init
    get all movies for the initial homepage view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Head from "next/head";
+import Image from "next/image";
 
-import styles from '@/styles/Home.module.css';
+import styles from "@/styles/Home.module.css";
 
-export default function Home() {
+const BASE_API = "https://wookie.codesubmit.io/movies";
+const BASE_API_KEY = "Wookie2021";
+
+export default function Home({ data }: any) {
+  console.log(data);
   return (
     <div className={styles.container}>
       <Head>
@@ -16,58 +20,26 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{` `}
-          <code className={styles.code}>src/pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=typescript-nextjs-starter"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className={styles.title}>{data?.movies?.[0].title}</h1>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=typescript-nextjs-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{` `}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <span className={styles.logo}>
+          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+        </span>
       </footer>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch(BASE_API, {
+    method: "get",
+    headers: new Headers({
+      Authorization: `Bearer ${BASE_API_KEY}`,
+    }),
+  });
+  const data = await response.json();
+
+  return { props: { data } };
 }
